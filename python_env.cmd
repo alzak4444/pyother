3e8333d679a56ba175e395009052afaa3751059c
albertusz <albertusz@yahoo.com>
2021-01-13 11:40:58 +0800

    a

--- python_env.cmd
+++ python_env.cmd
@@ -1,4 +1,7 @@
-rem set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Shared\Python37_64\
-cd c:\pyother
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+rem turn off execution alias in windows 10 from app & features settings
+set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Shared\Python37_64\
+set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Shared\Python37_64\Scripts
+cd f:\pyother
+rem call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmd /k
