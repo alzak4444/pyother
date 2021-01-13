a87b6e8f57c374eada6ce4002feaf21edfe130a4
albertusz <albertusz@yahoo.com>
2021-01-13 10:38:20 +0800

    a

---
+++ python_env.cmd
@@ -0,0 +1,4 @@
+rem set PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Shared\Python37_64\
+cd c:\pyother
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+cmd /k
